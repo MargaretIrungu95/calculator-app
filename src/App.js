b1a96dadfbdbf603036217e086aa6f474b749544
Margaret Irungu <mwirungu22@gmail.com>
2023-09-03 12:43:40 +0100

    final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { evaluate } from "mathjs";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [add, setAdd] = useState("");
+  const [ans, setAns] = useState("");
+
+  const allButtons = ["7", "8", "9", "C", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "=", "sqrt", "(", ")", "ans", "^"];
+
+  const handleClick =(string) => {
+    if(string === "="){
+        let expression = add;
+        setAdd(evaluate(expression));
+      try {
+        const res = evaluate(expression);
+        setAns (res.toString());
+      }
+      catch (error) {
+          setAns ("Error");
+      }
+  
+    } else if (string === "C"){
+      setAdd("");
+      setAns("");
+    } else if (string === "(") {
+      setAdd(add + "(");
+    } else if (string === ")") {
+      setAdd(add + ")");
+    }else if (string === "^") {
+      setAdd(add + "^");
+    }else if (string === "ans") {
+      setAdd(add + ans);
+    }else if (string === "sqrt") {
+      setAdd(add + "sqrt(");
+    }
+    else{
+      setAdd(add + string);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="resultDisplay">{add}</h1>
+      <div className={"btnWrap"}>
+        {
+          allButtons.map((button, index) =>{
+            return (
+              <button  className="calcButtons"  onClick={() => handleClick(button)} key={index}>{button}</button>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
-
 export default App;
